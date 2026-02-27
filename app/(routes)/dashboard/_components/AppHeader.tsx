be9d1b5cae99bf224bc0fbb0e4df840c54c37ed2
adepurahul493@gmail.com <adepurahul493@gmail.com>
2026-02-27 15:00:41 +0530

    today file modified

--- app/(routes)/dashboard/_components/AppHeader.tsx
+++ app/(routes)/dashboard/_components/AppHeader.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const menuOptions=[
     {
@@ -30,11 +32,13 @@ function AppHeader() {
     <div className='flex items-center justify-between p-4 border-b border-gray-200 ' >
       <Image src={'logo.svg'} alt='logo' width={180} height={90} />
       <div className='flex gap-5 items-center' >
-        {menuOptions.map((option,index)=>(
-            <div key={index}>
-                <h2 className='hover:font-bold cursor-pointer transition-all'>{option.name}</h2>
-            </div>
-        ))}
+{menuOptions.map((option) => (
+  <Link key={option.id} href={option.path}>
+    <h2 className='hover:font-bold cursor-pointer transition-all'>
+      {option.name}
+    </h2>
+  </Link>
+))}
       </div>
       <UserButton />
     </div>
